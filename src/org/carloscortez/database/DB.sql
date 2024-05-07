0c61c22cfc5104d470f8d54db5194fe994d887f8
JoseCortezKinal <josecortezkinal@gmail.com>
2024-05-07 08:58:16 -0600

    DB updated

--- src/org/carloscortez/database/DB.sql
+++ src/org/carloscortez/database/DB.sql
@@ -7,12 +7,12 @@ use EjemploDragDrop;
 create table Productos(
 	productoId int not null auto_increment,
     nombre varchar(30) not null,
-    imagen BLOB,
+    imagen LONGBLOB,
     primary key Imagen(productoId)
 );
 
 delimiter $$
-create procedure sp_agregarProducto(nom varchar(30), img BLOB)
+create procedure sp_agregarProducto(nom varchar(30), img LONGBLOB)
 begin
 	insert into Productos(nombre, imagen) values
 		(nom, img);
@@ -20,13 +20,14 @@ end $$
 delimiter ;
 
 delimiter $$
-create procedure sp_buscarProducto(imgId int)
+create procedure sp_buscarProducto(proId int)
 begin
-	select I.nombre, I.imagen from imagen I
-		where imagenId = imgId;
+	select P.productoId, P.nombre, P.imagen from Productos P
+		where productoId = proId;
 end $$
 delimiter ;
 
+select * from Productos;
 
-
+call sp_buscarProducto(1);
 
